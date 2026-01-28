3f66ef7455735e39891493875234c4989d5320d9
thecodingcube77 <thecodingcube77@gmail.com>
2026-01-28 23:18:59 +0100

    Code Generator start

---
+++ src/main/java/generator/CodeGenerator.java
@@ -0,0 +1,28 @@
+package generator;
+
+public class CodeGenerator {
+    public CodeGenerator() {
+        loadConfiguration();
+        applyConfiguration();
+        generateSetupFunction();
+        generateLoopFunction();
+    }
+
+    private void loadConfiguration() {
+
+    }
+
+    private void applyConfiguration() {
+
+    }
+
+    private void generateSetupFunction() {
+
+    }
+
+    private void generateLoopFunction() {
+
+    }
+
+    // Weitere Methoden für andere Steps der Codegenerierung
+}
